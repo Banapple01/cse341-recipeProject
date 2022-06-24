71061a0018eb0d36e74532325a218faf1290da3c
ZachRochette <zachrochette@hotmail.com>
2022-06-24 01:54:36 -0600

    fixing format with perttier

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 app
-    .use(bodyParser.json())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-    })
-    .use('/', indexRoutes)
-    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', indexRoutes)
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-    // don't forget to add env vars into heroku
+// don't forget to add env vars into heroku
 mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else {
-        app.listen(port);
-        console.log(`Connected to DB and listening on ${port}`);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
