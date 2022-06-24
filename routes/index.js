f9a52ac483d8566f964fecc9c39fc3f418fbfef9
Jonathan Wright <wrightjonathan99@gmail.com>
2022-06-24 17:16:29 -0600

    added file to views folder
    also changed some comments in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
+
+// un-comment the line below when the controllers have been made for the routes in recipeRoutes.js
 // router.use('/recipe', require('./recipeRoutes'));
 
 module.exports = router;
