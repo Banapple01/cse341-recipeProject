be87fdc17314e4fb825ff6b0b61116edb2caadbf
Jonathan Wright <wrightjonathan99@gmail.com>
2022-06-29 11:30:15 -0600

    added delete route
    also fixed the format prettier command in package.json. Added my test routes to routes.rest

--- controllers/recipeControllers.js
+++ controllers/recipeControllers.js
@@ -1,1 +1,27 @@
+// const { response } = require('express');
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
 // controller functions must be created!
+
+// delete a recipe
+const deleteRecipe = async (req, res) => {
+    try {
+        const id = new ObjectId(req.params.id);
+        const result = await mongodb
+            .getDb()
+            .db()
+            .collection('recipes')
+            .deleteOne({ _id: id }, true);
+        if (result.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(result.error || 'Something went wrong while deleting the recipe.');
+        }
+    } catch (err) {
+        res.status(500).json(err || 'Some error occurred while deleting the recipe.');
+    }
+};
+
+// remember to add your function to the exports!
+module.exports = { deleteRecipe };
