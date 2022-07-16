c8f335a5c4d9ffc29ac7d83b2ccae1d6542f5013
Jonathan Wright <wrightjonathan99@gmail.com>
2022-07-16 16:36:36 -0600

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,16 +1,16 @@
-const swaggerAutogen = require("swagger-autogen")();
+const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
     info: {
         title: 'My API',
         description: 'Recipes'
     },
-    host: 'cse341-recipe-app.herokuapp',
+    host: 'cse341-recipe-app.herokuapp.com',
     schemes: ['https']
 };
 
-const outputFile = "./swagger.json";
-const endpointsFiles = ["./routes/index.js"];
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
 
 /* NOTE: if you use the express Router, you must pass in the 
     'endpointsFiles' only the root file where the route starts,
