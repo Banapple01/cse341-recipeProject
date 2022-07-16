f7f67f02883e56ff5b93d967833e928484d6b3ea
Jonathan Wright <wrightjonathan99@gmail.com>
2022-07-16 15:44:32 -0600

    Update recipeControllers.js

--- controllers/recipeControllers.js
+++ controllers/recipeControllers.js
@@ -2,40 +2,35 @@
 const { calculateObjectSize } = require('bson');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
-const RecipeModel = require("../models/recipes");
+const RecipeModel = require('../models/recipes');
 
 // controller functions must be created!
 
 // Get all recipes
 async function getRecipe(req, res) {
-  try {
-    const result = await mongodb
-      .getDb()
-      .db("teamRecipePeeps")
-      .collection("recipes")
-      .find();
-    result.toArray().then((lists) => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).json(lists);
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        const result = await mongodb.getDb().db('teamRecipePeeps').collection('recipes').find();
+        result.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 }
 
 // Get single recipe
 async function getSingleRecipe(req, res, next) {
-    try { 
+    try {
         const userId = new ObjectId(req.params.id);
-        const result = await mongodb
-        .getDb
-        .db('teamRecipePeeps')
-        .collection('recipes')
-        .find({_id: userId});
+        const result = await mongodb.getDb
+            .db('teamRecipePeeps')
+            .collection('recipes')
+            .find({ _id: userId });
         result.toArray().then((lists) => {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(lists);
-        })
+        });
     } catch (err) {
         res.status(500).json(err);
     }
@@ -43,48 +38,42 @@ async function getSingleRecipe(req, res, next) {
 
 // POST create recipe
 const createRecipe = (req, res) => {
-  try {
-    //create new recipe
-    const recipe = {
-      name: req.body.name,
-      email: req.body.email,
-      desc: req.body.desc,
-      ingredients: req.body.ingredients,
-    };
+    try {
+        //create new recipe
+        const recipe = {
+            name: req.body.name,
+            email: req.body.email,
+            desc: req.body.desc,
+            ingredients: req.body.ingredients
+        };
 
-    //Save new recipe and respond
-    RecipeModel.create(recipe, (err, result) => {
-      if (err) console.log(err);
-      res.json(result);
-    });
-  } catch (err) {
-    res.status(500).json(err || "Could not create recipe");
-  }
+        //Save new recipe and respond
+        RecipeModel.create(recipe, (err, result) => {
+            if (err) console.log(err);
+            res.json(result);
+        });
+    } catch (err) {
+        res.status(500).json(err || 'Could not create recipe');
+    }
 };
 
 // delete a recipe
 const deleteRecipe = async (req, res) => {
-  try {
-    const id = new ObjectId(req.params.id);
-    const result = await mongodb
-      .getDb()
-      .db()
-      .collection("recipes")
-      .deleteOne({ _id: id }, true);
-    if (result.deletedCount > 0) {
-      res.status(204).send();
-    } else {
-      res
-        .status(500)
-        .json(
-          result.error || "Something went wrong while deleting the recipe."
-        );
+    try {
+        const id = new ObjectId(req.params.id);
+        const result = await mongodb
+            .getDb()
+            .db()
+            .collection('recipes')
+            .deleteOne({ _id: id }, true);
+        if (result.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(result.error || 'Something went wrong while deleting the recipe.');
+        }
+    } catch (err) {
+        res.status(500).json(err || 'Some error occurred while deleting the recipe.');
     }
-  } catch (err) {
-    res
-      .status(500)
-      .json(err || "Some error occurred while deleting the recipe.");
-  }
 };
 
 // remember to add your function to the exports!
